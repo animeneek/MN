e3569b0e61c2cf391bfd4689d2a86161fc52343f
animeneek <imemyanime@gmail.com>
2025-04-20 17:03:54 +0400

    Update app-search.js

--- app-search.js
+++ app-search.js
@@ -265,4 +265,12 @@ document.addEventListener('DOMContentLoaded', async () => {
       searchMovies(currentQuery, currentPage, true);
     }
   });
+
+  // Inject footer.html
+fetch('footer.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('footer-placeholder').innerHTML = data;
+  });
+
 });
